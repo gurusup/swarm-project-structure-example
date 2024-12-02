d670fb3f2f7ad7f340932ba8ce4f89e1ae666ffb
Víctor Mollá <victormolla14@gmail.com>
2024-12-02 09:15:13 +0100

    fix structure

--- configs/tools/switchers.py
+++ configs/tools/switchers.py
@@ -1,10 +1,13 @@
 from configs.agents.types import *
 
 def switch_to_availability_agent():
+    from configs.agents.types import availability_agent
     return availability_agent
 
 def switch_to_cancellation_agent():
+    from configs.agents.types import cancellation_agent
     return cancellation_agent
 
 def switch_to_reviews_agent():
+    from configs.agents.types import reviews_agent
     return reviews_agent
